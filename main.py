5c433d120c278f9afb044b9745d8ea4999a35cf9
AndreSandblom <andre.sandblom0257@stud.hkr.se>
2024-04-18 09:23:43 +0200

    fixed flake8

--- main.py
+++ main.py
@@ -8,41 +8,43 @@ def main():
     for key, value in num_char.items():
         print(f'The letter {key} was found {value} times')
 
+
 def get_num_words(text):
     words = text.split()
     return len(words)
+
 
 def count_letters(text):
     test_lower = text.lower()
     characters = [char for char in test_lower]
     letters = {
-        'a':0,
-        'b':0,
-        'c':0,
-        'd':0,
-        'e':0,
-        'f':0,
-        'g':0,
-        'h':0,
-        'i':0,
-        'j':0,
-        'k':0,
-        'l':0,
-        'm':0,
-        'n':0,
-        'o':0,
-        'p':0,
-        'q':0,
-        'r':0,
-        'e':0,
-        's':0,
-        't':0,
-        'u':0,
-        'v':0,
-        'w':0,
-        'x':0,
-        'y':0,
-        'z':0,
+        'a': 0,
+        'b': 0,
+        'c': 0,
+        'd': 0,
+        'e': 0,
+        'f': 0,
+        'g': 0,
+        'h': 0,
+        'i': 0,
+        'j': 0,
+        'k': 0,
+        'l': 0,
+        'm': 0,
+        'n': 0,
+        'o': 0,
+        'p': 0,
+        'q': 0,
+        'r': 0,
+        'e': 0,
+        's': 0,
+        't': 0,
+        'u': 0,
+        'v': 0,
+        'w': 0,
+        'x': 0,
+        'y': 0,
+        'z': 0,
     }
     for c in characters:
         for v in letters:
@@ -50,7 +52,10 @@ def count_letters(text):
                 letters[v] += 1
     return letters
 
+
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+
 main()
